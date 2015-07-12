7b1f999c9b27e307f3c54f190372e0c3c2505f21
Andrew Alexander <andrewalex.1992@gmail.com>
2015-07-12 15:33:57 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,119 @@
+twitchy_the_bot
+===============
+
+A reddit bot that gets twitch.tv streams from wiki pages and adds them to the subreddit's sidebar if they are live. 
+
+##Setup
+add your bots details and the subreddit into config.py, then run setup.py
+
+    python setup.py
+
+This will create the following wiki pages (customizable in default_wiki_config.json)
+    
+    # Twitchy will log errors in the history function of the wiki
+    /wiki/twitchbot_error_log 
+
+    # The imported config from default_wiki_config.json
+    /wiki/twitchbot_config 
+
+    # A list of banned twitch.tv usernames seperated by newlines
+    /wiki/banned_streams 
+
+     # A list of twitch.tv usernames seperated by newlines
+    /wiki/streams
+
+setup.py will also print out a string for you to put into your sidebar to allow people to PM the bot their livestreams in the correct format:
+
+`http://www.reddit.com/message/compose?to={username}&subject=Twitch.tv+request+%2Fr%2F{subreddit}&message=http%3A%2F%2Fwww.twitch.tv%2F{username}`
+
+with your bots username and the subreddit it's running in (taken from config.py) substituted where marked.
+
+### OAuth configuration
+The use of OAuth in PRAW requires a praw.ini file located in your config folder (varies by platform) or in the root directory if this project (overrides global settings)
+
+No matter where you put it, you need a praw.ini file with the following included:
+```
+[DEFAULT]
+domain: www.reddit.com
+oauth_client_id: <client_id>
+oauth_client_secret: <secret_access_key>
+oauth_redirect_uri: http://127.0.0.1:65010/authorize_callback
+oauth_refresh_token: <refresh_token>
+```
+#### Authenticating with OAuth
+
+There a few steps to authenticate via OAuth:
+1. Get the `client_id` and `secret_access_key` from [Reddit](https://www.reddit.com/prefs/apps/)
+2. Copy `client_id` to praw.ini as `oath_client_id` and `secret_access_key` as `oauth_client_secret` in praw.ini
+3. Run `authenticate.py`. It will open up your default web browser and present a page to grant access to the application
+4. You will be redirected to the url at `oauth_redirect_uri` in praw.ini. If you leave the default value, you will likely get a page not found error. This is okay. All you need is the code at the end of URL: `http://127.0.0.1:65010/authorize_callback?state=obtainingAuthentication&code=THIS_IS_THE_CODE_YOU_WANT`
+5. Copy the code into the prompt and press enter
+6. The script gets the refresh token and prints it to stdout. Copy the refresh token to praw.ini as `oauth_refresh_token`
+
+Now you will be good to go! You will not have to re-run authenticate.py unless you de-authorize your application from Reddit or you allow your refresh token to expire.
+
+See [here](http://praw.readthedocs.org/en/latest/pages/oauth.html) for more details on the PRAW implementation of OAuth
+
+
+###twitchbot_config
+
+All of the following config is editable in default_wiki_config.json before you run setup.py, and after you've ran setup.py the bot will pull it from `/wiki/twitchbot_config`
+
+    {
+    "max_streams_displayed":"12",
+    "max_title_length":"50",
+    "thumbnail_size":{
+        "width":"80",
+        "height":"50"
+    },
+    "stream_marker_start":"[](#startmarker)",
+    "stream_marker_end":"[](#endmarker)",
+    "string_format":"> 1. **[{name}](http://twitch.tv/{name})** -**{viewercount} Viewers**\n[{title}](http://twitch.tv/{name})\n",
+    "no_streams_string":"**No streams are currently live.**\n",
+    "wikipages":{
+        "error_log":"twitchbot_error_log",
+        "stream_list":"streams",
+        "ban_list":"banned_streams"
+    },
+    "allowed_games":[],
+    "messages":{
+        "success":"Your stream will be added to the list of livestreams in the sidebar, it will display the next time you are live on twitch.tv.\n\nProblems? [Contact the moderators here](http://www.reddit.com/message/compose?to=%2Fr%2F{subreddit})\n\n Do not reply to this message.",
+        "banned":"Sorry, but that stream is banned from this subreddit. If you feel this is an incorrect ban, [please message the moderators here](http://www.reddit.com/message/compose?to=%2Fr%2F{subreddit})\n\n Do not reply to this message.",
+        "already_exists":"Your stream is already in the list of livestreams that this bot checks. If you have just messaged your stream, please wait 5-10 minutes for the sidebar to update.\n\n Problems? Contact the moderators [here](http://www.reddit.com/message/compose?to=%2Fr%2F{subreddit})\n\n Do not reply to this message."
+    }
+    }
+
+### Config information
+
+`string_format` is the format that each twitch stream will be displayed as in your sidebar - `{name}` is the users twitch.tv username, `{viewercount}` is the viewer count on twitch.tv & `{title}` is the title they've set on twitch.tv (which can be limited in length with `max_title_length`)
+
+`no_streams_string` is the string that will be displayed if there are no streams live. 
+
+`thumbnail_size` is the width/height of the thumbnails that will be uploaded in a spritesheet to your subreddit. Make sure you have less than 50 images uploaded to your stylesheet, or the bot will not be able to upload the thumbnails.
+
+`stream_marker_start` and `stream_marker_end` are the two markers you must put in your sidebar for the bot to work. They indicate where the livestreams will appear. The bot will not function without them and will log errors to `/wiki/twitchbot_error_log` if it can't find either marker.
+
+`max_streams_displayed` is the limit for the amount of streams that can be displayed in your sidebar at any point.
+
+`messages` are the messages the bot will send if someone PMs it to add a stream - `success` is sent when they send a stream and it is added to `/wiki/streams`, `banned` is if they send a stream that is in `/wiki/banned_streams`, `already_exists` is if they send a stream that already exists in `/wiki/streams`
+
+`wikipages` is the location of the error log, stream list and ban list on your subreddit.
+
+By default, the bot will display any streams that are present in `/wiki/streams` - you can restrict it to display only certain games by adding to the `allowed_games` list. For example:
+
+    "allowed_games":["Bad rats", "Farming simulator 2014", "Goat simulator"]
+
+The games are not case sensitive, but they must match the game being played on twitch.tv to be allowed onto the sidebar - for example, someone playing "Bad rats: Rats revenge" would not be displayed, but "bad rats" would. 
+
+
+###Running
+
+Run the script with 
+
+    python twitchy.py
+
+It's recommended to run it on a timed basis, every 5-10 minutes.
+
+#Contact 
+
+If you have any issues with this bot, you can message me on reddit at /u/andygmb
---
+++ twitchy.py
@@ -0,0 +1,202 @@
+import requests, re, time, praw, HTMLParser, json, webbrowser
+from config import subreddit
+# from PIL import Image
+from StringIO import StringIO
+
+
+class configuration():
+	def __init__(self):
+		self.r, self.subreddit = self.reddit_setup()
+		self.config = self.get_config()
+		self.streams = self.wikipage_check(self.config["wikipages"]["stream_list"])
+		self.banned = self.wikipage_check(self.config["wikipages"]["ban_list"])
+
+	# self.messages = self.check_inbox()
+
+	def get_config(self):
+		try:
+			config = self.r.get_wiki_page(self.subreddit, "twitchbot_config").content_md
+			try:
+				config = json.loads(config)
+			except ValueError:
+				print "No JSON object could be found, or the config page has broken JSON.\nUse www.jsonlint.com to validate your wiki config page."
+				self.wikilog("No JSON object could be decoded from twitchbot config wiki page.")
+				raise
+			return HTMLParser.HTMLParser().unescape(config)
+		except requests.exceptions.HTTPError:
+			print "Couldn't access config wiki page, reddit may be down."
+			self.config = {"wikipages": {"error_log": "twitchbot_error_log"}}
+			self.wikilog("Couldn't access config wiki page, reddit may be down.")
+			raise
+
+	def wikilog(self, error):
+		self.r.edit_wiki_page(self.subreddit, self.config["wikipages"]["error_log"], error, error)
+
+	def reddit_setup(self):
+		print "Authenticating..."
+		r = praw.Reddit("Sidebar livestream updater for /r/{}".format(subreddit))
+		access_information = r.refresh_access_information()
+
+		sub = r.get_subreddit(subreddit)
+		return r, sub
+
+	def wikipage_check(self, wikipage):
+		try:
+			wiki_list = self.r.get_wiki_page(self.subreddit, wikipage).content_md.splitlines()
+			results = [item.lower() for item in wiki_list if len(item)]
+		except requests.exceptions.HTTPError:
+			print "No wikipage found at http://www.reddit.com/r/{}/wiki/{}".format(self.subreddit.display_name,
+																				   wikipage)
+			self.wikilog("Couldn't access wikipage at /wiki/{}/".format(wikipage))
+			results = []
+		return results
+
+	def check_inbox(self):
+		streams = []
+		inbox = self.r.get_inbox()
+		print "Checking inbox for new messages"
+		for message in inbox:
+			if message.new \
+					and message.subject == "Twitch.tv request /r/{}".format(self.subreddit):
+				message_content = message.body.split()[0]
+				try:
+					re_pattern = 'twitch.tv/(\w+)'
+					# pattern matches twitch username in the first group
+					re_result = re.search(re_pattern, message_content)
+					if re_result:
+						stream_name = re_result.group(1).lower()
+					# extract the username stored in regex group 1
+					else:
+						print "Could not find stream name in message."
+						continue  # skip to next message
+				except ValueError:
+					message.mark_as_read()
+					stream_name = "null"
+					print "Could not find stream name in message."
+
+				if "twitch.tv/" in message_content \
+						and len(stream_name) <= 25 \
+						and stream_name not in self.banned \
+						and stream_name not in self.streams:
+					streams.append(stream_name)
+					message.reply(self.config["messages"]["success"].format(subreddit=self.subreddit))
+					message.mark_as_read()
+
+				elif stream_name in self.banned:
+					message.reply(self.config["messages"]["banned"].format(subreddit=self.subreddit))
+					message.mark_as_read()
+
+				elif stream_name in self.streams:
+					message.reply(self.config["messages"]["already_exists"].format(subreddit=self.subreddit))
+					message.mark_as_read()
+		if streams:
+			new_streams = list(set([stream for stream in streams if stream not in [self.streams, self.banned]]))
+			self.streams.extend(new_streams)
+			self.subreddit.edit_wiki_page(
+				self.config["wikipages"]["stream_list"],
+				"\n".join(self.streams),
+				reason="Adding stream(s): " + ", ".join(new_streams)
+			)
+
+	def update_sidebar(self):
+		print "Updating sidebar"
+
+		# Get current content so we don't lose it later
+		sidebar = self.r.get_settings(self.subreddit)
+		submit_text = HTMLParser.HTMLParser().unescape(sidebar["submit_text"])
+		desc = HTMLParser.HTMLParser().unescape(sidebar['description'])
+		try:
+			start = desc.index(self.config["stream_marker_start"])
+			end = desc.index(self.config["stream_marker_end"]) + len(self.config["stream_marker_end"])
+		except ValueError:
+			self.wikilog("Couldn't find the stream markers in the sidebar.")
+			raise
+
+		# build stream string
+		livestreams_string = "".join(livestreams.streams).encode("ascii", "ignore")
+		print livestreams_string
+		desc = desc.replace(
+			desc[start:end],
+			"{} {} {}".format(self.config["stream_marker_start"] + '\n', livestreams_string,
+							  self.config["stream_marker_end"])
+		)
+		self.subreddit.update_settings(description=desc.encode('utf8'), submit_text=submit_text)
+
+
+class livestreams():
+	def __init__(self, config):
+		self.config = config
+		self.streams = []
+		self.thumbnails = []
+
+	def check_stream_length(self):
+		max_streams = int(self.config.config["max_streams_displayed"])
+		if len(self.streams) > max_streams:
+			self.streams = self.streams[:max_streams]
+			self.thumbnails = self.thumbnails[:max_streams]
+			print "There are more than {max_stream_count} streams currently \
+			- the amount displayed has been reduced to {max_stream_count}. \
+			You can increase this in your config.py file.".format(max_stream_count=max_streams)
+		if len(self.streams):
+			return True
+		else:
+			self.streams = self.config.config["no_streams_string"]
+			return False
+
+	def get_livestreams(self):
+
+		print "Requesting stream info"
+		api_link = "https://api.twitch.tv/kraken/streams?game=Heroes of Newerth"
+
+		try:
+			data = requests.get(api_link).json()
+			print data
+			if data["_total"] > 0:
+				self.parse_stream_info(data)
+			else:
+				pass
+		except:
+			pass
+
+	def parse_stream_info(self, data):
+		print "Parsing stream info"
+
+		# number to pre-pend to stream name
+		current_number = 1
+		allowed_games = [str(game.lower()) for game in self.config.config["allowed_games"]]
+		
+		for streamer in data["streams"]:
+			if not len(allowed_games) or streamer["game"].lower() in allowed_games:
+				game = streamer["game"].lower()
+				title = streamer["channel"]["status"]
+
+				# Removing characters that can break reddit formatting
+				title = re.sub(r'[*)(>/#\[\]\\]*', '', title)
+				title = title.replace("\n", "")
+
+				# Add ellipses if title is too long
+				if len(title) >= int(self.config.config["max_title_length"]):
+					title = title[0:int(self.config.config["max_title_length"]) - 3] + "..."
+				name = streamer["channel"]["name"].encode("utf-8")
+				display_name = streamer["channel"]["display_name"].encode("utf-8")
+				viewer_count = "{:,}".format(streamer["viewers"])
+				self.thumbnails.append(streamer["preview"]["template"])
+
+				# actually add the formatted stream name to the list of streams
+				self.streams.append(
+					HTMLParser.HTMLParser().unescape(
+						self.config.config["string_format"].format(name=name, title=title, viewercount=viewer_count,
+																   display_name=display_name,
+																   current_number=current_number))
+				)
+                current_number += 1
+
+
+if __name__ == "__main__":
+	config = configuration()
+	livestreams = livestreams(config)
+	livestreams.get_livestreams()
+	if livestreams.check_stream_length():
+		livestreams.config.update_sidebar()
+	else:
+		livestreams.config.update_sidebar()
